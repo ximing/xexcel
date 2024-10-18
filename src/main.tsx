1b5ca8b185b6c841aec2682e1ff2ad2e8a34d6fa
ximing <ximing@meituan.com>
2024-10-18 18:35:37 +0800

    fix(io): exceljs 改 vendor script + registerExcelJS DI（pre-bundle 浏览器 load 挂死）

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app/App'
+import { setupExcelJS } from './app/exceljs'
 import './app/style.css'
+
+setupExcelJS()
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
